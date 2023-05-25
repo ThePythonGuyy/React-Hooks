9de3d854912b49145abbf13e261a429c31e86ec4
ThePythonGuyy <thepythonguy@outlook.com>
2023-05-25 08:40:45 +0530

    useState objects

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -6,12 +6,13 @@ const UseState = () => {
   const [count, setCount] = useState(0);
   const initialState = 0;
   const [count1, setCount1] = useState(initialState);
+  const [name, setName] = useState({ fstName: "", lstName: "" });
 
   const incFive = () => {
-    for(let i=0; i<5; i++) {
-      setCount1(prevState => prevState + 1)
+    for (let i = 0; i < 5; i++) {
+      setCount1((prevState) => prevState + 1);
     }
-  }
+  };
 
   return (
     <Div>
@@ -27,11 +28,33 @@ const UseState = () => {
 
       <Div2>
         <span>count {count1}</span>
-        <button onClick={() => setCount1(prevState => prevState + 1)}>Inc</button>
-        <button onClick={() => setCount1(prevState => prevState - 1)}>Dec</button>
+        <button onClick={() => setCount1((prevState) => prevState + 1)}>
+          Inc
+        </button>
+        <button onClick={() => setCount1((prevState) => prevState - 1)}>
+          Dec
+        </button>
         <button onClick={() => setCount1(initialState)}>Reset</button>
         <button onClick={incFive}>incFive</button>
       </Div2>
+
+      <Form>
+        <input
+          type="text"
+          placeholder="Firstname"
+          value={name.fstName}
+          onChange={(e) => setName({ ...name, fstName: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Lastname"
+          value={name.lstName}
+          onChange={(e) => setName({ ...name, lstName: e.target.value })}
+        />
+
+        <h2>Your first name is - {name.fstName}</h2>
+        <h2>Your last name is - {name.lstName}</h2>
+      </Form>
     </Div>
   );
 };
@@ -54,7 +77,13 @@ const Div1 = styled.div``;
 const Div2 = styled.div`
   display: flex;
   gap: 5px;
-  span{
+  span {
     margin-right: 10px;
+  }
+`;
+
+const Form = styled.form`
+  input{
+    margin-left: 10px;
   }
 `;
